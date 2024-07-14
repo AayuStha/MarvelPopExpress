bfa874d85c21221ea34f41756acca2aaee85617a
AayuStha <aayustha21@gmail.com>
2024-07-14 22:14:22 +0545

    fixed addtocart btn

--- script.js
+++ script.js
@@ -4,11 +4,9 @@ const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT;
-app.set('views', path.join(__dirname, 'views'));
-
-app.use(express.static("public"))
 const featuredItems = require('./data/featured.json');
 
+app.set('views', path.join(__dirname, 'views'));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
@@ -17,6 +15,8 @@ app.get("/", (req, res) => {
 });
 
 app.listen(PORT, (err) => {
-    if (err) console.error(`Error starting server on port ${PORT}`, err);
-    else console.log(`Server rendered at port ${PORT}`);
+    if (err)
+        console.error(`Error starting server on port ${PORT}`, err);
+    else 
+        console.log(`Server rendered at port ${PORT}`);
 });
