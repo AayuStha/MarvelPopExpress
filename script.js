68841746f0f276322edc47dd1ff32b7545dc9171
Aayush Shrestha <aayustha21@gmail.com>
2024-07-19 22:29:15 +0545

    added reviews

--- script.js
+++ script.js
@@ -5,13 +5,14 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT;
 const featuredItems = require('./data/featured.json');
+const reviews = require('./data/reviews.json');
 
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-    res.render('index', { featuredItems });
+    res.render('index', { featuredItems , reviews});
 });
 
 app.listen(PORT, (err) => {
