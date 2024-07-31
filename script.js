52f813297c8f5731e577373aa7ada85b01059c42
Aayush Shrestha <aayustha21@gmail.com>
2024-07-31 21:15:09 +0545

    added contact page with firebase db

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const express = require('express');
 const http = require('http');
 require('dotenv').config();
 const path = require('path');
+const bodyParser = require('body-parser');
+const { database } = require('./src/config/firebase');
+const { ref, push } = require('firebase/database');
 
 const app = express();
 const server = http.createServer(app);
@@ -9,13 +12,43 @@ const PORT = process.env.PORT;
 const featuredItems = require('./data/featured.json');
 const reviews = require('./data/reviews.json');
 
-app.set('views', path.join(__dirname, 'views'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(express.static("public"));
+
 app.set("view engine", "ejs");
+app.set('views', path.join(__dirname, 'views'));
+
 
 app.get("/", (req, res) => {
     res.render('index', { featuredItems , reviews});
 });
+
+app.get('/contact', (req, res) => {
+    res.render('contact', { message: null, error: null });
+});
+
+app.post('/submit-contact', (req, res) => {
+    const { name, email, subject, enquiryType, message } = req.body;
+  
+    const contactsRef = ref(database, `contacts/${name}`);
+  
+    push(contactsRef, {
+      name,
+      email,
+      subject,
+      enquiryType,
+      message,
+    })
+    .then(() => {
+        res.render('contact', { message: `Thank you ${name}, for your message! We will get back to you soon.`, error: null });
+      })
+      .catch((error) => {
+        console.error("Error saving contact information:", error);
+        res.render('contact', { message: null, error: "There was an error submitting your message. Please refresh the page and try again." });
+      });
+    });
+
 
 server.listen(PORT, (err) => {
     if (err)
---
+++ src/config/firebase.js
@@ -0,0 +1,21 @@
+const { initializeApp } = require('firebase/app');
+const { getDatabase } = require('firebase/database');
+require('dotenv').config();
+
+const firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Get a reference to the database
+const database = getDatabase(app);
+
+module.exports = { database };
