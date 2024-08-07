536efc61b799eb3694c1e21bfa8950e14d44d84a
Aayush Shrestha <aayustha21@gmail.com>
2024-08-07 18:27:02 +0545

    addded responsiveness and about us page

--- script.js
+++ script.js
@@ -29,25 +29,29 @@ app.get('/contact', (req, res) => {
 });
 
 app.post('/submit-contact', (req, res) => {
-    const { name, email, subject, enquiryType, message } = req.body;
-  
-    const contactsRef = ref(database, `contacts/${name}`);
-  
-    push(contactsRef, {
-      name,
-      email,
-      subject,
-      enquiryType,
-      message,
-    })
-    .then(() => {
-        res.render('contact', { message: `Thank you ${name}, for your message! We will get back to you soon.`, error: null });
-      })
-      .catch((error) => {
-        console.error("Error saving contact information:", error);
-        res.render('contact', { message: null, error: "There was an error submitting your message. Please refresh the page and try again." });
-      });
-    });
+  const { name, email, subject, enquiryType, message } = req.body;
+
+  const contactsRef = ref(database, `contacts/${name}`);
+
+  push(contactsRef, {
+    name,
+    email,
+    subject,
+    enquiryType,
+    message,
+  })
+  .then(() => {
+      res.render('contact', { message: `Thank you ${name}, for your message! We will get back to you soon.`, error: null });
+  })
+  .catch((error) => {
+    console.error("Error saving contact information:", error);
+    res.render('contact', { message: null, error: "There was an error submitting your message. Please refresh the page and try again." });
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render('about');
+});
 
 
 server.listen(PORT, (err) => {
