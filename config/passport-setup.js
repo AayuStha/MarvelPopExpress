4412127afd7b9af9a654875c0e0b0897751e3b1a
Aayush Shrestha <aayustha21@gmail.com>
2024-08-18 22:17:12 +0545

    Update passport-setup.js

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -6,7 +6,7 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: process.env.CALLBACKURL
+    callbackURL: "http://marvelpop.aayushrestha.com/auth/google/callback"
   },
   async (accessToken, refreshToken, profile, done) => {
     try {
