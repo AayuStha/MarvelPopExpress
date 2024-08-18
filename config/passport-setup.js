a7bb9c408433aefed25f81ac127093617e0ccda9
Aayush Shrestha <aayustha21@gmail.com>
2024-08-18 22:13:15 +0545

    fixed auth issues

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -6,7 +6,7 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3001/auth/google/callback"
+    callbackURL: process.env.CALLBACKURL
   },
   async (accessToken, refreshToken, profile, done) => {
     try {
