1e899bceda3553c3cb00ea6d2f12f8f716f6d306
Aayush Shrestha <aayustha21@gmail.com>
2024-08-19 13:03:19 +0545

    fixing prod /dashboard redirect

--- script.js
+++ script.js
@@ -7,14 +7,23 @@ const multer = require('multer');
 const upload = multer({ storage: multer.memoryStorage() });
 const passport = require('passport');
 const session = require('express-session');
-const { database, ref } = require('./config/firebase'); // Import database and ref
-const { set } = require('firebase/database'); // Import the set function
-
+const RedisStore = require('connect-redis')(session);
+const redis = require('redis');
+const { database, ref } = require('./config/firebase');
+const { set } = require('firebase/database');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
-// Determine if the app is running in production or development
 const isProduction = process.env.NODE_ENV === 'production';
 const callbackURL = isProduction ? process.env.CALLBACK_URL_PROD : process.env.CALLBACK_URL_DEV;
+
+const redisClient = redis.createClient({
+  host: 'localhost',  // Adjust this if Redis is running on a different host
+  port: 6379
+});
+
+redisClient.on('error', (err) => {
+  console.error('Redis error: ', err);
+});
 
 // Passport configuration for Google OAuth
 passport.use(new GoogleStrategy({
@@ -25,17 +34,13 @@ passport.use(new GoogleStrategy({
   },
   async function(accessToken, refreshToken, profile, done) {
     try {
-      // Create a reference to the user data in the Realtime Database
       const userRef = ref(database, 'users/' + profile.id);
-
-      // Save user data to Firebase Realtime Database
       await set(userRef, {
         googleId: profile.id,
         displayName: profile.displayName,
         email: profile.emails[0].value,
         photoUrl: profile.photos[0].value
       });
-
       done(null, profile);
     } catch (error) {
       console.error('Error saving user to Firebase:', error);
@@ -44,7 +49,6 @@ passport.use(new GoogleStrategy({
   }
 ));
 
-// Serialize and deserialize user to manage sessions
 passport.serializeUser((user, done) => {
     done(null, user);
 });
@@ -57,21 +61,23 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-// Middleware
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(upload.array());  // for parsing multipart/form-data
+
+// Configure session middleware to use Redis store
 app.use(session({
+    store: new RedisStore({ client: redisClient }),
     secret: process.env.SESSION_SECRET,
     resave: false,
-    saveUninitialized: true,
+    saveUninitialized: false,
     cookie: { secure: isProduction }
 }));
+
 app.use(passport.initialize());
 app.use(passport.session());
 
-// Define routes
 app.get("/", (req, res) => {
     const featuredItems = require('./data/featured.json');
     const reviews = require('./data/reviews.json');
@@ -81,13 +87,12 @@ app.get("/", (req, res) => {
 app.get('/auth/google',
   passport.authenticate('google', { scope: ['profile', 'email'] }));
 
-  app.get('/auth/google/callback', 
-    passport.authenticate('google', { failureRedirect: '/login' }),
-    (req, res) => {
-      console.log('Google authentication successful. Redirecting to /dashboard.');
-      res.redirect('/dashboard');
-  });
-  
+app.get('/auth/google/callback', 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  (req, res) => {
+    console.log('Google authentication successful. Redirecting to /dashboard.');
+    res.redirect('/dashboard'); 
+});
 
 app.get("/login", (req, res) => {
     res.render('login');
@@ -109,7 +114,6 @@ app.get('/faq', (req, res) => {
     res.render('faq');
 });
 
-// HTTP server
 const server = http.createServer(app);
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
